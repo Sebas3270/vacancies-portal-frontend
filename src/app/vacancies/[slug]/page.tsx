991f1583cd6649303d82ec390c4d76f98a1975ee
Sebastian <salvarezocampo@hotmail.com>
2023-11-20 01:20:54 -0600

    adding dynamic metadata to vacancy page

--- src/app/vacancies/[slug]/page.tsx
+++ src/app/vacancies/[slug]/page.tsx
@@ -1,11 +1,29 @@
 import { getVacancy } from '@/database'
 import { NextPageProps } from '@/types'
+import { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
 interface Props extends NextPageProps {
     params: { slug: string } 
 }
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+    
+    const id = params.slug
+    const { vacancy } = await getVacancy(id);
+
+    const title = vacancy ? `${vacancy.title} - Sebas Solutions` : 'Vacancy not found - Sebas Solutions'
+    const description = vacancy ? `${vacancy.description}` : 'Vacancy not found, description not available'
+   
+    return {
+      title,
+      description
+    }
+  }
 
 export default async function Page(props: Props) {
 
